b700a5c710995f2b5c8a4090be7fa62b2a9025f5
Muhammad Khizar <mkhizar2001@gmail.com>
2020-10-04 18:23:32 -0500

    Huge E-Commerce Angular Project!
    Mega folder containing all files to my biggest personal project yet!

---
+++ E-Commerce/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAndaw8IAp9k9P6UG21golNDP280MiEXd8",
+    authDomain: "ecom-app1.firebaseapp.com",
+    databaseURL: "https://ecom-app1.firebaseio.com",
+    projectId: "ecom-app1",
+    storageBucket: "",
+    messagingSenderId: "48888800599",
+    appId: "1:48888800599:web:77f49b24e85aa934" 
+  }
+};
---
+++ E-Commerce/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAndaw8IAp9k9P6UG21golNDP280MiEXd8",
+    authDomain: "ecom-app1.firebaseapp.com",
+    databaseURL: "https://ecom-app1.firebaseio.com",
+    projectId: "ecom-app1",
+    storageBucket: "",
+    messagingSenderId: "48888800599",
+    appId: "1:48888800599:web:77f49b24e85aa934"  
+  }
+};
